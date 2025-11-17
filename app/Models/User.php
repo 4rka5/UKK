7773587ea68e6b7489d0153eac6a1b8c7346b7d1
4rka5 <4rka5@github.com>
2025-11-17 14:48:16 +0700

    Add HasFactory trait to User model for seeding

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
+    use HasFactory, HasApiTokens;
 
     protected $fillable = ['username','fullname','email','password','role','status'];
     protected $hidden = ['password','remember_token'];
